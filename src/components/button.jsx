2ff33d7e33eae11e6a78d2209d2704c4deb60be7
Vigneshkalvium <vipvignesh.dr.2244@gmail.com>
2025-03-06 10:40:13 +0530

    task completed

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,13 @@
-// write button card here
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <button className='bg-blue-500 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:bg-blue-700 hover:scale-105'>
+        View Product
+      </button>
+    </div>
+  )
+}
+
+export default Button
